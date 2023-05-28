35081f118a75de5ecfb3c82a2d83fc83ac22ae92
pjoterini <piotrekjan.gorski@gmail.com>
2023-05-28 10:49:27 +0200

    fix auth protected route

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,145 +1,141 @@
-import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { EditDeletePostButtons } from "./EditDeletePostButtons";
-import { UpdootSection } from "./UpdootSection";
-import NextLink from "next/link";
+import { ChatIcon, ChevronDownIcon, RepeatIcon } from '@chakra-ui/icons'
+import { Box, Button, Flex, Heading, Stack, Text } from '@chakra-ui/react'
+import { Form, Formik } from 'formik'
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import {
   MeQuery,
-  Post,
   useCreateReplyMutation,
-  useRepliesQuery,
-} from "../generated/graphql";
-import { ChatIcon, ChevronDownIcon, RepeatIcon } from "@chakra-ui/icons";
-import { SubmitBtn } from "./SubmitBtn";
-import { Formik, Form } from "formik";
-import { useRouter } from "next/router";
-import { useIsAuth } from "../utils/useIsAuth";
-import { InputField } from "./InputField";
-import ReplyComponent from "./ReplyComponent";
+  useRepliesQuery
+} from '../generated/graphql'
+import { EditDeletePostButtons } from './EditDeletePostButtons'
+import { InputField } from './InputField'
+import ReplyComponent from './ReplyComponent'
+import { SubmitBtn } from './SubmitBtn'
+import { UpdootSection } from './UpdootSection'
 
 interface PostComponentProps {
   post: {
-    id: number;
-    createdAt: string;
-    updatedAt: string;
-    title: string;
-    category: string;
-    points: number;
-    textSnippet: string;
-    voteStatus?: number | null | undefined;
+    id: number
+    createdAt: string
+    updatedAt: string
+    title: string
+    category: string
+    points: number
+    textSnippet: string
+    voteStatus?: number | null | undefined
     creator: {
-      __typename?: "User" | undefined;
-      id: number;
-      username: string;
-    };
-  };
-  meData: MeQuery | undefined;
+      __typename?: 'User' | undefined
+      id: number
+      username: string
+    }
+  }
+  meData: MeQuery | undefined
 }
 
 const PostComponent = ({ post, meData }: PostComponentProps) => {
-  // REPLY FORM, CREATE REPLY
-  const router = useRouter();
-  useIsAuth();
-  const [, createReply] = useCreateReplyMutation();
+  const router = useRouter()
+  const [, createReply] = useCreateReplyMutation()
+  const [showReplyInputs, setShowReplyInputs] = useState(false)
 
-  const [showReplyInputs, setShowReplyInputs] = useState(false);
-  let replyInputs;
+  let replyInputs
   showReplyInputs
     ? (replyInputs = (
         <Formik
-          initialValues={{ text: "", postid: post.id }}
+          initialValues={{ text: '', postid: post.id }}
           onSubmit={async (values) => {
-            const { error } = await createReply({ input: values });
-            if (error?.message.includes("not authenticated")) {
-              router.push("/login");
+            const { error } = await createReply({ input: values })
+            if (error?.message.includes('not authenticated')) {
+              router.push('/login')
             } else {
-              setShowReplyInputs((prev) => !prev);
+              setShowReplyInputs((prev) => !prev)
             }
           }}
         >
           {({ isSubmitting }) => (
             <Form>
-              <Box mx="auto" width="96%" mt={4}>
+              <Box mx='auto' width='96%' mt={4}>
                 <InputField
                   textarea
-                  name="text"
-                  placeholder="text"
-                  label="Reply"
+                  name='text'
+                  placeholder='text'
+                  label='Reply'
                   maxLength={400}
                 />
-                <Box display="none">
+                <Box display='none'>
                   <InputField
                     readOnly
                     value={post.id}
-                    name="postid"
-                    label="postid"
+                    name='postid'
+                    label='postid'
                   />
                 </Box>
               </Box>
-              <Flex mb={4} justify="center" align="center">
+              <Flex mb={4} justify='center' align='center'>
                 <SubmitBtn
-                  text="Create Reply"
+                  text='Create Reply'
                   state={isSubmitting}
                   confirmation={false}
-                  action="create"
-                  type="reply"
+                  action='create'
+                  type='reply'
                 />
               </Flex>
             </Form>
           )}
         </Formik>
       ))
-    : null;
+    : null
   // SHOW REPLIES, PAGINATION
-  const [answerCountFive, setAnswersCountFive] = useState(false);
+  const [answerCountFive, setAnswersCountFive] = useState(false)
   const [variables, setVariables] = useState({
     limit: 5,
     cursor: null as null | string,
-    postid: post.id,
-  });
+    postid: post.id
+  })
   const [{ data, error, fetching }] = useRepliesQuery({
-    variables,
-  });
+    variables
+  })
 
-  const [showReplies, setShowReplies] = useState(false);
-  let show;
-  showReplies ? (show = "") : (show = "none");
+  const [showReplies, setShowReplies] = useState(false)
+  let show
+  showReplies ? (show = '') : (show = 'none')
 
   if (!fetching && !data) {
     return (
       <Box>
-        <Text color="white">
+        <Text color='white'>
           you got query for replies failed for some reason
         </Text>
-        ;<Text color="white">{error?.message}</Text>
+        ;<Text color='white'>{error?.message}</Text>
       </Box>
-    );
+    )
   }
 
   return (
     <Box
-      width="100%"
-      bg="primary"
-      shadow="md"
-      borderColor="accent"
-      borderWidth="1px"
-      borderRadius="5px"
+      width='100%'
+      bg='primary'
+      shadow='md'
+      borderColor='accent'
+      borderWidth='1px'
+      borderRadius='5px'
     >
-      <Flex w="100%" alignItems="center" justifyContent="space-between">
+      <Flex w='100%' alignItems='center' justifyContent='space-between'>
         <UpdootSection post={post} />
 
         <Flex
-          flexDir={{ base: "column", sm: "row" }}
-          w="100%"
-          justifyContent="space-between"
+          flexDir={{ base: 'column', sm: 'row' }}
+          w='100%'
+          justifyContent='space-between'
         >
-          <Flex mt={6} ml={4} flexDir="column">
-            <NextLink href="/post/[id]" as={`/post/${post.id}`}>
-              <Heading color="white" fontSize="lg" fontWeight="medium">
+          <Flex mt={6} ml={4} flexDir='column'>
+            <NextLink href='/post/[id]' as={`/post/${post.id}`}>
+              <Heading color='white' fontSize='lg' fontWeight='medium'>
                 {post.title}
               </Heading>
             </NextLink>
-            <Text my={4} fontWeight="light" color="white">
+            <Text my={4} fontWeight='light' color='white'>
               {post.textSnippet}..
             </Text>
           </Flex>
@@ -148,18 +144,18 @@ const PostComponent = ({ post, meData }: PostComponentProps) => {
             my={5}
             mx={5}
             flexShrink={0}
-            flexDir="column"
-            justifyContent="center"
-            alignItems="center"
-            color="white2"
-            fontSize="xs"
+            flexDir='column'
+            justifyContent='center'
+            alignItems='center'
+            color='white2'
+            fontSize='xs'
           >
             <Box>POSTED BY</Box>
-            <Text pb={2} fontSize="md" color="green">
+            <Text pb={2} fontSize='md' color='green'>
               {post.creator.username}
             </Text>
             <Box>CATEGORY</Box>
-            <Text pb={2} fontSize="md" color="green">
+            <Text pb={2} fontSize='md' color='green'>
               {post.category}
             </Text>
             {meData?.me?.id !== post.creator.id ? null : (
@@ -169,29 +165,29 @@ const PostComponent = ({ post, meData }: PostComponentProps) => {
         </Flex>
       </Flex>
 
-      <Flex color="white2" alignItems="center" justifyContent="center">
+      <Flex color='white2' alignItems='center' justifyContent='center'>
         <ChatIcon h={4} />
-        <Text color="white" ml={1} mr={8} mb={1}>
+        <Text color='white' ml={1} mr={8} mb={1}>
           &#40;
-          {(data && data.replies.hasMore) || answerCountFive ? ">" : ""}
+          {(data && data.replies.hasMore) || answerCountFive ? '>' : ''}
           {answerCountFive ? 5 : data?.replies.replies.length}
           &#41;
         </Text>
         {data?.replies.replies.length !== 0 ? (
           <Button
             onClick={() => {
-              setShowReplies((prev) => !prev);
+              setShowReplies((prev) => !prev)
             }}
           >
             <Text
               _hover={{
-                color: "white",
+                color: 'white'
               }}
               mb={1}
             >
               See Answers
             </Text>
-            <ChevronDownIcon color="white" h={7} w={7} />
+            <ChevronDownIcon color='white' h={7} w={7} />
           </Button>
         ) : (
           <Text mb={1}>Be first to answer!</Text>
@@ -199,12 +195,12 @@ const PostComponent = ({ post, meData }: PostComponentProps) => {
 
         <Button
           onClick={() => {
-            setShowReplyInputs((prev) => !prev);
+            setShowReplyInputs((prev) => !prev)
           }}
-          color="green"
+          color='green'
           ml={5}
         >
-          <Text color="white" mb={1} mr={2}>
+          <Text color='white' mb={1} mr={2}>
             Reply
           </Text>
           <RepeatIcon />
@@ -212,13 +208,13 @@ const PostComponent = ({ post, meData }: PostComponentProps) => {
       </Flex>
       {replyInputs}
       {fetching && !data ? (
-        <Text color="white">loading...</Text>
+        <Text color='white'>loading...</Text>
       ) : (
         <Stack
           style={{
             display: `${show}`,
-            marginInline: "auto",
-            width: "96%",
+            marginInline: 'auto',
+            width: '96%'
           }}
         >
           {data!.replies.replies.map((reply) =>
@@ -235,38 +231,38 @@ const PostComponent = ({ post, meData }: PostComponentProps) => {
         <Flex>
           <Button
             style={{
-              display: `${show}`,
+              display: `${show}`
             }}
             onClick={() => {
-              setAnswersCountFive(true);
+              setAnswersCountFive(true)
               setVariables({
                 limit: variables.limit,
                 cursor:
                   data.replies.replies[data.replies.replies.length - 1]
                     .createdAt,
-                postid: variables.postid,
-              });
+                postid: variables.postid
+              })
             }}
             isLoading={fetching}
             _hover={{
-              bgColor: "green",
-              borderColor: "green",
-              color: "white",
+              bgColor: 'green',
+              borderColor: 'green',
+              color: 'white'
             }}
             mb={2}
             px={6}
-            mx="auto"
-            type="submit"
-            color="white"
-            borderColor="green"
-            border="1px solid white"
+            mx='auto'
+            type='submit'
+            color='white'
+            borderColor='green'
+            border='1px solid white'
           >
             Load Others
           </Button>
         </Flex>
       ) : null}
     </Box>
-  );
-};
+  )
+}
 
-export default PostComponent;
+export default PostComponent
